8a07df3e573d63d7cf1dda797ff4b18073e37e58
Sarah Peiffer <sarahpeiffer@microsoft.com>
2021-09-01 14:00:33 -0700

    saving changes

--- sarah-test-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
+++ sarah-test-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
+# Note - This script used in the pipeline as inline script
 
+# These are plain pipeline variable which can be modified anyone in the team
+# AGENT_RELEASE=ciev2
+# AGENT_IMAGE_TAG_SUFFIX=08202021
+
+#Name of the ACR for ciprod & cidev images
 DESTINATION_ACR_NAME=containerinsightsprod
 AGENT_IMAGE_FULL_PATH=${ACR_NAME}/public/azuremonitor/containerinsights/${AGENT_RELEASE}:${AGENT_RELEASE}${AGENT_IMAGE_TAG_SUFFIX}
 AGENT_IMAGE_TAR_FILE_NAME=agentimage.tar.gz
@@ -26,43 +32,34 @@ if [ -z ${TAG_NAME+x} ]; then
     exit 1;
 fi
 
-if [ -z ${DESTINATION_FILE_NAME+x} ]; then
-    echo "DESTINATION_FILE_NAME is unset, unable to continue"
+if [ -z ${AGENT_IMAGE_TAR_FILE_NAME+x} ]; then
+    echo "AGENT_IMAGE_TAR_FILE_NAME is unset, unable to continue"
     exit 1;
 fi
 
-echo "Folder Contents"
-ls
-
-apt update
-apt-get install -y unzip wget gzip
-
-
+#Install crane
+echo "Installing crane"
+wget -O crane.tar.gz https://github.com/google/go-containerregistry/releases/download/v0.4.0/go-containerregistry_Linux_x86_64.tar.gz
+tar xzvf crane.tar.gz
+echo "Installed crane"
 
 echo "Login cli using managed identity"
 az login --identity
 
-TMP_FOLDER=$(mktemp -d)
-cd $TMP_FOLDER
-
-echo "Downloading docker tarball image from $TARBALL_IMAGE_FILE_SAS"
-wget -O $DESTINATION_FILE_NAME "$TARBALL_IMAGE_FILE_SAS"
 
 echo "Getting acr credentials"
 TOKEN_QUERY_RES=$(az acr login -n "$DESTINATION_ACR_NAME" -t)
 TOKEN=$(echo "$TOKEN_QUERY_RES" | jq -r '.accessToken')
 DESTINATION_ACR=$(echo "$TOKEN_QUERY_RES" | jq -r '.loginServer')
-/package/unarchive/src/Shell/crane auth login "$DESTINATION_ACR" -u "00000000-0000-0000-0000-000000000000" -p "$TOKEN"
+./crane auth login "$DESTINATION_ACR" -u "00000000-0000-0000-0000-000000000000" -p "$TOKEN"
 
-DEST_IMAGE_FULL_NAME="$DESTINATION_ACR_NAME.azurecr.io/$IMAGE_NAME:$TAG_NAME"
-
-if [[ "$DESTINATION_FILE_NAME" == *"tar.gz"* ]]; then
-  gunzip $DESTINATION_FILE_NAME
+if [[ "$AGENT_IMAGE_TAR_FILE_NAME" == *"tar.gz"* ]]; then
+  gunzip $AGENT_IMAGE_TAR_FILE_NAME
 fi
 
-if [[ "$DESTINATION_FILE_NAME" == *"tar.zip"* ]]; then
-  unzip $DESTINATION_FILE_NAME
+if [[ "$AGENT_IMAGE_TAR_FILE_NAME" == *"tar.zip"* ]]; then
+  unzip $AGENT_IMAGE_TAR_FILE_NAME
 fi
 
-echo "Pushing file $TARBALL_IMAGE_FILE to $DEST_IMAGE_FULL_NAME"
-/package/unarchive/src/Shell/crane push *.tar "$DEST_IMAGE_FULL_NAME"
+echo "Pushing file $TARBALL_IMAGE_FILE to $AGENT_IMAGE_FULL_PATH"
+./crane push *.tar "$AGENT_IMAGE_FULL_PATH"
