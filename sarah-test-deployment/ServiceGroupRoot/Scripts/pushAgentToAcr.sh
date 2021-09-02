1bae2eabc9ace1190add31567e15df48f6fb7cf3
Sarah Peiffer <sarahpeiffer@microsoft.com>
2021-09-01 17:56:01 -0700

    updated script and removed infra artifacts

--- sarah-test-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
+++ sarah-test-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
@@ -4,37 +4,32 @@ set -e
 # Note - This script used in the pipeline as inline script
 
 # These are plain pipeline variable which can be modified anyone in the team
-# AGENT_RELEASE=ciev2
+# AGENT_RELEASE=ciprod
 # AGENT_IMAGE_TAG_SUFFIX=08202021
 
 #Name of the ACR for ciprod & cidev images
-DESTINATION_ACR_NAME=containerinsightsprod
-AGENT_IMAGE_FULL_PATH=${ACR_NAME}/public/azuremonitor/containerinsights/${AGENT_RELEASE}:${AGENT_RELEASE}${AGENT_IMAGE_TAG_SUFFIX}
-AGENT_IMAGE_TAR_FILE_NAME=agentimage.tar.gz
-
-if [ -z ${DESTINATION_ACR_NAME+x} ]; then
-    echo "DESTINATION_ACR_NAME is unset, unable to continue"
-    exit 1;
-fi
-
-if [ -z ${TARBALL_IMAGE_FILE_SAS+x} ]; then
-    echo "TARBALL_IMAGE_FILE_SAS is unset, unable to continue"
-    exit 1;
-fi
-
-if [ -z ${IMAGE_NAME+x} ]; then
-    echo "IMAGE_NAME is unset, unable to continue"
-    exit 1;
-fi
-
-if [ -z ${TAG_NAME+x} ]; then
-    echo "TAG_NAME is unset, unable to continue"
-    exit 1;
-fi
+ACR_NAME=containerinsightsprod
+LINUX_AGENT_IMAGE_FULL_PATH=${ACR_NAME}/public/azuremonitor/containerinsights/${AGENT_RELEASE}:${AGENT_RELEASE}${AGENT_IMAGE_TAG_SUFFIX}
+WINDOWS_AGENT_IMAGE_FULL_PATH=${ACR_NAME}/public/azuremonitor/containerinsights/${AGENT_RELEASE}:win-${AGENT_RELEASE}${AGENT_IMAGE_TAG_SUFFIX}
 
 if [ -z ${AGENT_IMAGE_TAR_FILE_NAME+x} ]; then
     echo "AGENT_IMAGE_TAR_FILE_NAME is unset, unable to continue"
     exit 1;
+fi
+
+if [ -z $AGENT_IMAGE_TAG_SUFFIX ]; then
+  echo "-e error value of AGENT_IMAGE_TAG_SUFFIX variable shouldnt be empty"
+  exit 1
+fi
+
+if [ ${#AGENT_IMAGE_TAG_SUFFIX} -ne 8 ]; then
+  echo "-e error length of AGENT_IMAGE_TAG_SUFFIX should be 8. Make sure it is in MMDDYYYY format"
+  exit 1
+fi
+
+if [ -z $AGENT_RELEASE ]; then
+  echo "-e error AGENT_RELEASE shouldnt be empty"
+  exit 1
 fi
 
 #Install crane
@@ -43,12 +38,12 @@ wget -O crane.tar.gz https://github.com/google/go-containerregistry/releases/dow
 tar xzvf crane.tar.gz
 echo "Installed crane"
 
-echo "Login cli using managed identity"
+#echo "Login cli using managed identity"
 az login --identity
 
 
 echo "Getting acr credentials"
-TOKEN_QUERY_RES=$(az acr login -n "$DESTINATION_ACR_NAME" -t)
+TOKEN_QUERY_RES=$(az acr login -n "$ACR_NAME" -t)
 TOKEN=$(echo "$TOKEN_QUERY_RES" | jq -r '.accessToken')
 DESTINATION_ACR=$(echo "$TOKEN_QUERY_RES" | jq -r '.loginServer')
 ./crane auth login "$DESTINATION_ACR" -u "00000000-0000-0000-0000-000000000000" -p "$TOKEN"
